c6c0c6e4870cd0a20ff5c4dfc12d00eda472f699
suraj <alphaguy433@gmail.com>
2024-07-14 20:32:35 +0530

    Added only backend files to this branch

--- client/src/utilities/dateFormat.js
+++
@@ -1,6 +0,0 @@
-export const dateformat=(dateVal)=>{
-    const date = new Date(parseInt(dateVal))
-    const option = {day:'2-digit', month:'short', year:"numeric"}
-    return date.toLocaleDateString('en-US', option)
-}
-
